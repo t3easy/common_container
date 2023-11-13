caa708f434edd5048e7b33ee9497b4c6f1af2544
Jan Kiesewetter <jan@t3easy.de>
2023-11-13 10:01:44 +0100

    [BUGFIX] Don't override entire type tx_commoncontainer__grid_container

--- Configuration/TCA/Overrides/tt_content.grid_container.php
+++ Configuration/TCA/Overrides/tt_content.grid_container.php
@@ -17,7 +17,7 @@ call_user_func(static function () {
                             'colPos' => 200,
                         ],
                     ],
-                ]
+                ],
             )
         )
         ->setDefaultValues([
@@ -26,119 +26,107 @@ call_user_func(static function () {
         ->setIcon('EXT:container/Resources/Public/Icons/container-1col.svg')
     );
 
-    $GLOBALS['TCA']['tt_content']['types']['tx_commoncontainer__grid_container'] = [
-        'columnsOverrides' => [
-            't3easy_ce_classes' => [
-                'config' => [
-                    'items' => $typo3Version < 12 ? [
-                        [
-                            'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-x',
-                            'grid-x',
-                            '',
-                            'grid-type',
-                        ],
-                        [
-                            'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-y',
-                            'grid-y',
-                            '',
-                            'grid-type',
-                        ],
-
-                        [
-                            'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-margin-x',
-                            'grid-margin-x',
-                            '',
-                            'gutter-type',
-                        ],
-                        [
-                            'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-margin-y',
-                            'grid-margin-y',
-                            '',
-                            'gutter-type',
-                        ],
-                        [
-                            'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-padding-x',
-                            'grid-padding-x',
-                            '',
-                            'gutter-type',
-                        ],
-                        [
-                            'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-padding-y',
-                            'grid-padding-y',
-                            '',
-                            'gutter-type',
-                        ],
-                    ] : [
-                        [
-                            'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-x',
-                            'value' => 'grid-x',
-                            'group' => 'grid-type',
-                        ],
-                        [
-                            'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-y',
-                            'value' => 'grid-y',
-                            'group' => 'grid-type',
-                        ],
-
-                        [
-                            'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-margin-x',
-                            'value' => 'grid-margin-x',
-                            'group' => 'gutter-type',
-                        ],
-                        [
-                            'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-margin-y',
-                            'value' => 'grid-margin-y',
-                            'group' => 'gutter-type',
-                        ],
-                        [
-                            'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-padding-x',
-                            'value' => 'grid-padding-x',
-                            'group' => 'gutter-type',
-                        ],
-                        [
-                            'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-padding-y',
-                            'value' => 'grid-padding-y',
-                            'group' => 'gutter-type',
-                        ],
+    $GLOBALS['TCA']['tt_content']['types']['tx_commoncontainer__grid_container']['columnsOverrides'] = [
+        't3easy_ce_classes' => [
+            'config' => [
+                'items' => $typo3Version < 12 ? [
+                    [
+                        'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-x',
+                        'grid-x',
+                        '',
+                        'grid-type',
                     ],
-                    'itemGroups' => [
-                        'grid-type' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.itemGroups.grid-type',
-                        'gutter-type' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.itemGroups.gutter-type',
+                    [
+                        'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-y',
+                        'grid-y',
+                        '',
+                        'grid-type',
                     ],
-                    'minitems' => 1,
-                    'default' => 'grid-x,grid-margin-x',
+
+                    [
+                        'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-margin-x',
+                        'grid-margin-x',
+                        '',
+                        'gutter-type',
+                    ],
+                    [
+                        'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-margin-y',
+                        'grid-margin-y',
+                        '',
+                        'gutter-type',
+                    ],
+                    [
+                        'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-padding-x',
+                        'grid-padding-x',
+                        '',
+                        'gutter-type',
+                    ],
+                    [
+                        'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-padding-y',
+                        'grid-padding-y',
+                        '',
+                        'gutter-type',
+                    ],
+                ] : [
+                    [
+                        'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-x',
+                        'value' => 'grid-x',
+                        'group' => 'grid-type',
+                    ],
+                    [
+                        'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-y',
+                        'value' => 'grid-y',
+                        'group' => 'grid-type',
+                    ],
+
+                    [
+                        'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-margin-x',
+                        'value' => 'grid-margin-x',
+                        'group' => 'gutter-type',
+                    ],
+                    [
+                        'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-margin-y',
+                        'value' => 'grid-margin-y',
+                        'group' => 'gutter-type',
+                    ],
+                    [
+                        'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-padding-x',
+                        'value' => 'grid-padding-x',
+                        'group' => 'gutter-type',
+                    ],
+                    [
+                        'label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.items.grid-padding-y',
+                        'value' => 'grid-padding-y',
+                        'group' => 'gutter-type',
+                    ],
                 ],
+                'itemGroups' => [
+                    'grid-type' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.itemGroups.grid-type',
+                    'gutter-type' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:t3easy_ce_classes.itemGroups.gutter-type',
+                ],
+                'minitems' => 1,
+                'default' => 'grid-x,grid-margin-x',
             ],
-            'layout' => [
-                'config' => [
-                    'items' => $typo3Version < 12 ? [
-                        ['LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.default_value', 0],
-                        ['LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:layout.items.1', 1],
-                        ['LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:layout.items.2', 2],
-                    ] : [
-                        ['label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.default_value', 'value' => 0],
-                        ['label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:layout.items.1', 'value' => 1],
-                        ['label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:layout.items.2', 'value' => 2],
-                    ],
+        ],
+        'layout' => [
+            'config' => [
+                'items' => $typo3Version < 12 ? [
+                    ['LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.default_value', 0],
+                    ['LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:layout.items.1', 1],
+                    ['LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:layout.items.2', 2],
+                ] : [
+                    ['label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.default_value', 'value' => 0],
+                    ['label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:layout.items.1', 'value' => 1],
+                    ['label' => 'LLL:EXT:common_container/Resources/Private/Language/locallang_ttc.xlf:layout.items.2', 'value' => 2],
                 ],
             ],
         ],
-        'showitem' => '
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-                --palette--;;general, header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header.ALT.div_formlabel,
-            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-                --palette--;;tx_commoncontainer__appearance,
-                --palette--;;appearanceLinks,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
-                --palette--;;language,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-                --palette--;;hidden,
-                --palette--;;access,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
-                categories,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
-                rowDescription,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended
-        ',
     ];
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        '--palette--;;tx_commoncontainer__appearance',
+        'tx_commoncontainer__grid_container',
+        'replace:--palette--;;frames'
+    );
 });
