779c65eebbd916181370842da8d8350fbe281366
Jan Kiesewetter <jan@t3easy.de>
2023-11-20 21:18:26 +0100

    [TASK] Encapsulate TCA overrides

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,8 +1,11 @@
 <?php
+
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'common_container',
-    'Configuration/PageTs/main.tsconfig',
-    'Common container'
-);
+call_user_func(static function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        'common_container',
+        'Configuration/PageTs/main.tsconfig',
+        'Common container'
+    );
+});
--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,8 +1,11 @@
 <?php
+
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'common_container',
-    'Configuration/TypoScript/',
-    'Common container'
-);
+call_user_func(static function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        'common_container',
+        'Configuration/TypoScript/',
+        'Common container'
+    );
+});
