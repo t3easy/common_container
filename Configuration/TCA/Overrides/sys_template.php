5684f6c893f560b4c3e7c3826ee1d3b59c8dcff7
Jan Kiesewetter <jan@t3easy.de>
2023-11-30 13:14:40 +0100

    [TASK] Restructure TypoScript

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -5,7 +5,13 @@ defined('TYPO3') or die();
 call_user_func(static function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         'common_container',
-        'Configuration/TypoScript/',
-        'Common container'
+        'Configuration/TypoScript/Standalone/',
+        'Standalone'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        'common_container',
+        'Configuration/TypoScript/WithFluidStyledContent/',
+        'Together with Fluid Content Elements'
     );
 });
